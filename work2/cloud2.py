33eb1448fca64f511a0715e1d3d9dcae89c8ee02
eddiez9 <eddiezhang9@hotmail.com>
2016-05-25 00:00:11 +1000

    sim working

--- work2/cloud2.py
+++ work2/cloud2.py
@@ -5,8 +5,8 @@ import math
 
 # This function convert entries of ratings.csv into (userid -> (movieid, rating))
 def parse_rating(line):
-	fields = re.findall('("[^"]+"|[^,]+)', line.strip())
-	return (int(fields[0]), (int(fields[1]), float(fields[2])))
+	user_id, movie_id, rating, timestamp = line.strip().split(",")
+	return (int(user_id.strip()), (int(movie_id.strip()), float(rating)))
 
 # This function converts entries of movies.csv into (movieid -> title)
 def parse_movie(line):
@@ -27,16 +27,19 @@ def calc_avg(line):
 	return(uid, (sm/count))
 
 ### NEIGHBORHOOD SIM ###
+
 # Set variables ACD for use
-def do_precalculation(line):
+# userID -> ([(movieID, rating)],average)
+def do_precalculation(i, line):
 	ratings = line[1][0]
 	average = line[1][1]
 	user_sim = []
 	ratei = -1 
+
 	# if this user has rated i
 	for r in ratings:
 		if r[0] == i:
-			ratei = r[0]
+			ratei = r[1]
 
 	# don't calculate unless the user has rated i
 	if ratei != -1:
@@ -51,13 +54,15 @@ def do_precalculation(line):
 				b = r[1] - average
 				d = b ** 2
 				numerator = a * b
+				
 				# add to rdd
-				temp_tup = ((i,j),(numerator,c,d))
+				temp_tup = ( (i,j) , (numerator,c,d) )
 				user_sim.append(temp_tup)
-	return user_sim
+		return tuple(user_sim)
+	return ()
 
 # Final calculations
-def add_topbottom(a,b):
+def add_topbottom(a, b):
 	numa, ca, da = a
 	numb, cb, db = b
 	return(numa+numb, ca+cb, da+db)
@@ -67,9 +72,16 @@ def do_final_calc(line):
 	num, c, d = line[1]
 	c = math.sqrt(c)
 	d = math.sqrt(d)
-	return (line[0], num/(c*d))
+	final_calc = []
+	if c*d != 0:
+		final_calc.append((line[0][1], (line[0][0],num/(c*d))))
+	return tuple(final_calc)
 
-
+def keep_topten(line):
+	top_ten = []
+	data = line[1]
+	top_ten= sorted(data, reverse=True, key=lambda tup: tup[1])[:10]
+	return (line[0],top_ten)
 
 if __name__ == "__main__":
 	sc = SparkContext(appName="Workload2")
@@ -95,19 +107,24 @@ if __name__ == "__main__":
 	user_rating_sum_count = entries.map(lambda entry: (entry[0],(entry[1][1], 1))).reduceByKey(add_values)
 	user_rating_average = user_rating_sum_count.map(calc_avg)
 
+
 	# Stores userID -> ([(movieID, rating)],average)
 	user_aggregated = entries.groupByKey().join(user_rating_average).cache()
 
 	# Create set of movies
 	have_rated = pentries.map(lambda entry: entry[1][0]).collect()
-	# havent_rated = moventries.filter(filter_rated).collectAsMap()
 
-	# Get all (i,j variables)
+	# Get all ((i,j), variables)
 	pre_calc_sim_rdd = sc.emptyRDD()
 	for i in have_rated:
-		pre_calc_sim_rdd += (user_aggregated.flatMap(do_precalculation))
+		pre_calc_sim_rdd += user_aggregated.flatMap(lambda entry: do_precalculation(i, entry))
 	
 	# Aggregate by i,j and compute
-	sim_ij = pre_calc_sim_rdd.reduceByKey(add_topbottom).map(do_final_calc)
-	print(sim_ij.collect())
+	sim_ij = pre_calc_sim_rdd.reduceByKey(add_topbottom).flatMap(do_final_calc).take(10)
 	
+	#print(sim_ij.collect())
+	sim_ij.saveAsTextFile("test")
+
+
+
+
